b56253406fa8d940f859b2d8e26567f0c1063ae4
Alan Veloso <alantsv@gmail.com>
2020-10-22 14:00:55 -0300

    Change return of send funtion to bytes

--- core/Agent.sol
+++ core/Agent.sol
@@ -3,13 +3,15 @@ pragma solidity >=0.4.16 <0.8.0;
 
 contract Agent {
     
-    function send(address receiver, bytes memory message) public {
-        (bool success, ) = receiver.call(message);
+    function send(address receiver, bytes memory message) public returns (bytes memory){
+        (bool success, bytes memory reply) = receiver.call(message);
         require(success);
+        return reply;
     }
 
-    function send(address receiver, bytes memory message, uint _value) public {
-        (bool success, ) = receiver.call{value: _value}(message);
+    function send(address receiver, bytes memory message, uint _value) public returns (bytes memory){
+        (bool success, bytes memory reply) = receiver.call{value: _value}(message);
         require(success);
+        return reply;
     }
 }
